d2236f46352a77fb67677a4044382587377f3f1b
vhishikawa <victor.hishikawa17@gmail.com>
2025-08-19 08:14:58 -0300

    Feat: serviços rodando

--- spark_service/jupyter_notebook_config.py
+++ spark_service/jupyter_notebook_config.py
@@ -1,6 +1,1 @@
-# spark_service/jupyter_notebook_config.py
-c.ServerApp.base_url = '/jupyter/'
-c.ServerApp.token = ''
-c.ServerApp.password = ''
-c.ServerApp.allow_origin = '*'
-c.ServerApp.disable_check_xsrf = True
+c.NotebookApp.base_url = '/jupyter'
