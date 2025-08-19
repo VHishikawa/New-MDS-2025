e771d373b455f4c9796f22cdfb1e97be9e672e2e
vhishikawa <victor.hishikawa17@gmail.com>
2025-08-18 21:43:47 -0300

    Feat: nginx funcionando para alguns serviços

---
+++ spark_service/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+# spark_service/jupyter_notebook_config.py
+c.ServerApp.base_url = '/jupyter/'
+c.ServerApp.token = ''
+c.ServerApp.password = ''
+c.ServerApp.allow_origin = '*'
+c.ServerApp.disable_check_xsrf = True
